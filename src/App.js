24964103c7c1d1773f5226622062250a57acd096
Cblands <48147185+Cblands@users.noreply.github.com>
2021-01-12 22:41:53 -0800

    Create boiler plate for front end

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Stock Trader Application
+        <br/>
+        <br/>
+        Seng 468, Software Scalability
+      </header>
+    </div>
+  );
+}
+
+export default App;
